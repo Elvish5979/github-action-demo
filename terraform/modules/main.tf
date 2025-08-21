523c0fbd908783565571a8529bfc326299e194e2
vishal <elvishranmale@gmail.com>
2025-08-21 15:38:25 +0530

    update

--- terraform/modules/main.tf
+++
@@ -1,9 +0,0 @@
-module "secure_bucket" {
-  source             = "./modules/s3-bucket"
-  bucket_name        = "my-tf-example-bucket-bella"
-  versioning_enabled = true
-  tags = {
-    Environment = "dev"
-    Owner       = "Vishal"
-  }
-}
