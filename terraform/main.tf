3ea6cae9d0fb88c81b34cfe69ad304d2c6f56677
vishal <elvishranmale@gmail.com>
2025-08-21 15:31:54 +0530

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 module "secure_bucket" {
-  source             = "./modules/s3-bucket"
+  source             = "modules/s3-bucket"
   bucket_name        = "my-tf-example-bucket-bella"
   versioning_enabled = true
   tags = {
